ad261f373e2ad6a978fd3d7751798bd92f9df4db
NLOG <sorisem4106@naver.com>
2017-11-26 18:29:12 +0900

    Update Main.php

--- src/nlog/NLOGSmartUI/Main.php
+++ src/nlog/NLOGSmartUI/Main.php
@@ -19,8 +19,11 @@ use nlog\NLOGSmartUI\FormHandler\functions\PluginInfoFunction;
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
 use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Utils;
+use pocketmine\command\PluginCommand;
 
-class Main extends FormsID implements Listener {
+class Main extends PluginBase implements Listener {
 	
 	/** @var Settings */
 	public $setting;
@@ -43,6 +46,41 @@ class Main extends FormsID implements Listener {
 	
 	/** @var array */
 	public $simplearea = [];
+	
+	
+	
+	/** @var string */
+	private $link = "https://raw.githubusercontent.com/NLOGPlugins/NLOGSmartUI/master/version.json";
+	
+	#====================================================
+	
+	/** @var int */
+	public $MainID = 5215;
+	
+	/** @var int */
+	public $MenuID = 4315;
+	
+	#====================================================
+	
+	/** @var int */
+	public $WarpID = 8472;
+	
+	/** @var int */
+	public $SendMoneyID = 3837;
+	
+	/** @var int */
+	public $RecieveMoneyLoggerID = 8476;
+	
+	/** @var int */
+	public $AreaMoveID = 5648;
+	
+	/** @var int */
+	public $MonthCalendarID = 9373;
+	
+	/** @var int */
+	public $PluginInfoID = 3736;
+	
+	#====================================================
 	
 	public function onEnable() {
 		$this->checkUpdate();
@@ -93,6 +131,55 @@ class Main extends FormsID implements Listener {
 			}
 		}
 	}
+	
+	protected function checkUpdate(): bool {
+		$url = json_decode(Utils::getURL($this->link), true);
+		
+		if (!isset($url["last_ver"])) {
+			$this->getLogger()->alert("인터넷에 연결이 되지 않아 업데이트 확인을 할 수 없습니다.");
+			return false;
+		}
+		
+		$last_ver = $url["last_ver"];
+		$notice = $url["notice"];
+		$force = $url["require_force_update"];
+		$changelog = "";
+		foreach ($url["changelog"] as $ver => $content) {
+			$content = trim($content);
+			$changelog .= "v$ver : $content";
+		}
+		
+		if ($notice !== "") {
+			$this->getLogger()->alert("공지 사항 : " . $notice);
+		}
+		
+		if (version_compare($this->getDescription()->getVersion(), $last_ver) < 0) {
+			$this->getLogger()->alert("새로운 버전이 출시되었습니다. 현재 버전 : " . $this->getDescription()->getVersion() . " 최신 버전 : " . $last_ver);
+			if ($force) {
+				$this->getLogger()->alert("업데이트를 하지 않으면 플러그인을 사용할 수 없습니다.");
+				$this->getPluginLoader()->disablePlugin($this);
+				return false;
+			}
+			return false;
+		}
+		
+		$this->getLogger()->notice("최신 버전입니다.");
+		return true;
+	}
+	
+	protected function registerCommand(string $name, string $permission, string $description = "", string $usage = "", bool $force = false, array $aliases= []) {
+  	if ($force && $this->getServer()->getCommandMap()->getCommand($name) instanceof Command) {
+  		$this->getServer()->getCommandMap()->getCommand($name)->unregister($this->getServer()->getCommandMap());
+  	}
+  	$command = new PluginCommand($name, $this);
+  	$command->setLabel($name);
+  	$command->setPermission($permission);
+  	$command->setDescription($description);
+  	$command->setUsage($usage);
+  	$command->setAliases($aliases);
+  	
+  	$this->getServer()->getCommandMap()->register($this->getDescription()->getName(), $command);
+  }
 	
 	public function onCommand(CommandSender $sender,Command $command,string $label,array $args): bool {
 		if (!$sender instanceof Player) {
